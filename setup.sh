e539a2275eb2a9eac7f3ef5e0ca01a2dc6b5c5ad
FateAKong <azurelfsun@gmail.com>
2013-09-24 03:20:47 +0000

    Make minor changes to fit personal usage
    Remove Emacs-related packages installation and setup
    
    Modify git repository of .dotfiles

--- setup.sh
+++ setup.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
-# Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
-# for headless setup. 
+
+# Simple setup.sh for configuring Ubuntu EC2 instance for headless setup. 
+
+# Install git
+sudo apt-get install -y git
 
 # Install nvm: node-version manager
 # https://github.com/creationix/nvm
-sudo apt-get install -y git
-sudo apt-get install -y curl
 curl https://raw.github.com/creationix/nvm/master/install.sh | sh
 
-# Load nvm and install latest production node
+# Load nvm and install latest stable node
 source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
-
-# Install jshint to allow checking of JS code within emacs
-# http://jshint.com/
-npm install -g jshint
+nvm install stable
 
 # Install rlwrap to provide libreadline features with node
 # See: http://nodejs.org/api/repl.html#repl_repl
 sudo apt-get install -y rlwrap
 
-# Install emacs24
-# https://launchpad.net/~cassou/+archive/emacs
-sudo add-apt-repository -y ppa:cassou/emacs
-sudo apt-get -qq update
-sudo apt-get install -y emacs24-nox emacs24-el emacs24-common-non-dfsg
+# Install Heroku toolbelt
+# https://toolbelt.heroku.com/debian
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+
+# Install rlwrap to provide libreadline features with node
+# See: http://nodejs.org/api/repl.html#repl_repl
+sudo apt-get install -y rlwrap
 
 # Install Heroku toolbelt
 # https://toolbelt.heroku.com/debian
@@ -33,16 +31,10 @@ wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 # git pull and install dotfiles as well
 cd $HOME
-if [ -d ./dotfiles/ ]; then
-    mv dotfiles dotfiles.old
+if [ -d ./.dotfiles/ ]; then
+    mv .dotfiles .dotfiles.old
 fi
-if [ -d .emacs.d/ ]; then
-    mv .emacs.d .emacs.d~
-fi
-git clone https://github.com/startup-class/dotfiles.git
-ln -sb dotfiles/.screenrc .
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sf dotfiles/.emacs.d .
-
+git clone https://github.com/fateakong/.dotfiles.git
+ln -sb .dotfiles/.bash_profile .
+ln -sb .dotfiles/.bashrc .
+ln -sb .dotfiles/.bashrc_custom .
